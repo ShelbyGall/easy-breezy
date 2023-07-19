5639004e815191046c485b9ccde5ec91d892f21d
ShellTooSleepy <sjgallegos270@gmail.com>
2023-07-19 14:41:05 -0700

    geocoding implemented but not working properly

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const buttonEl = document.getElementById("search-button")
 const unitsList = document.getElementById("units-list")
 const unitSelEl = document.getElementById("unit-selector")
 const currTemp = document.getElementById("current-temp")
+const locationInput = document.getElementById("search-input")
 
 document.getElementById("kelvin").addEventListener("click", function() {
     unitSelected = ""
@@ -27,16 +28,35 @@ document.getElementById("metric").addEventListener("click", function() {
 
 async function getWeather(lat, lon, unit) {
     const response = await fetch(
-`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`
-    )
+`
+https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}
+`)
     const weather = await response.json()
     console.log(weather)
     const temp = Math.ceil(weather.main.temp)
     currTemp.textContent = `${temp}° ${abbrevUnit}`
 }
 
+async function getCoord(location) {
+
+    const response = await fetch(
+`
+http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}
+`)
+
+    const result = await response.json()
+    console.log(result)
+    let coordinates = {
+        lat: result[0].lat,
+        lon: result[0].lon
+    }
+
+    console.log(coordinates)
+
+    return coordinates
+}
+
 buttonEl.addEventListener("click", function() {
-    let lat = 34.686785
-    let lon = -118.154163
-    getWeather(lat, lon, unitSelected)
+    let coordinates = getCoord(locationInput.value)
+    getWeather(coordinates.lat, coordinates.lon, unitSelected)
 })
