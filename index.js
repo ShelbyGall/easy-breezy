217df10827e68faa802ca686c68135f41ca1aa9c
ShellTooSleepy <sjgallegos270@gmail.com>
2023-07-19 17:15:05 -0700

    getCoord working fine, must account for more cities

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const apiKey = "d3af1e291a1a85b8ae498f77c27c2d94"
 
 let unitSelected = "" 
-let abbrevUnit = ""
+let abbrevUnit = "K"
 
 const buttonEl = document.getElementById("search-button")
 const unitsList = document.getElementById("units-list")
@@ -32,7 +32,6 @@ async function getWeather(lat, lon, unit) {
 https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}
 `)
     const weather = await response.json()
-    console.log(weather)
     const temp = Math.ceil(weather.main.temp)
     currTemp.textContent = `${temp}° ${abbrevUnit}`
 }
@@ -45,18 +44,23 @@ http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKe
 `)
 
     const result = await response.json()
-    console.log(result)
     let coordinates = {
         lat: result[0].lat,
         lon: result[0].lon
     }
 
-    console.log(coordinates)
-
     return coordinates
 }
 
-buttonEl.addEventListener("click", function() {
-    let coordinates = getCoord(locationInput.value)
-    getWeather(coordinates.lat, coordinates.lon, unitSelected)
-})
+buttonEl.addEventListener("click", async function() {
+    try {
+        let coordinates = await getCoord(locationInput.value);
+        if (coordinates) {
+            await getWeather(coordinates.lat, coordinates.lon, unitSelected);
+        } else {
+            console.error("Coordinates not found.");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+});
