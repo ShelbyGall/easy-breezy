31c4ccd9af09b9adae515bd769acc16f249ce5b6
ShellTooSleepy <sjgallegos270@gmail.com>
2023-07-25 00:45:20 -0700

    display and select multiple location results

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const currTemp = document.getElementById("current-temp")
 const locationInput = document.getElementById("search-input")
 const weatherImg = document.getElementById("weather-img")
 const locationName = document.getElementById("location-name")
+const weatherResultList = document.getElementById("weather-tiles-result-list")
 
 document.getElementById("kelvin").addEventListener("click", function() {
     unitSelected = ""
@@ -28,15 +29,24 @@ document.getElementById("metric").addEventListener("click", function() {
 
 
 
-async function getWeather(lat, lon, unit) {
+async function getWeather(coordinates) {
+
+    let lat = coordinates.lat
+    let lon = coordinates.lon
+    let name = coordinates.name
+    let state = coordinates.state
     try {
         const response = await fetch(
         `
-        https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}
+        https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unitSelected}
         `)
         const weather = await response.json()
+
+        console.log(weather)
+
         const temp = Math.ceil(weather.main.temp)
         currTemp.textContent = `${temp}°${abbrevUnit}\n`
+        locationName.textContent = `${name}, ${state}`
         if (weather) {
             weatherImg.src = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`
         }
@@ -45,6 +55,8 @@ async function getWeather(lat, lon, unit) {
     }
 }
 
+
+
 async function getCoord(location) {
 
     const response = await fetch(
@@ -52,27 +64,36 @@ async function getCoord(location) {
 http://api.openweathermap.org/geo/1.0/direct?q=${location},&limit=10&appid=${apiKey}
 `)
 
-    const result = await response.json()
-    console.log(result)
-    let coordinates = {
-        lat: result[0].lat,
-        lon: result[0].lon
+    const results = await response.json()
+    
+    console.log(results)
+    
+    for(let currResult of results) {
+
+        let li = document.createElement("li")
+        li.textContent = `${currResult.name}, ${currResult.state} - Lat: ${currResult.lat} Lon: ${currResult.lon}`
+        li.data = {
+            name: currResult.name,
+            state: currResult.state,
+            lat: currResult.lat,
+            lon: currResult.lon
+        }
+
+        weatherResultList.appendChild(li)
+
+        li.addEventListener("click", function() {
+            getWeather(li.data)
+        })
     }
-
-    locationName.textContent = `${result[0].name}, ${result[0].state}`
-
-    return coordinates
 }
 
+
 buttonEl.addEventListener("click", async function() {
+    weatherResultList.innerHTML = ""
     try {
-        let coordinates = await getCoord(locationInput.value);
-        if (coordinates) {
-            await getWeather(coordinates.lat, coordinates.lon, unitSelected);
-        } else {
-            console.error("Coordinates not found.");
-        }
+        await getCoord(locationInput.value);
     } catch (error) {
         console.error("An error occurred:", error);
     }
 });
+
